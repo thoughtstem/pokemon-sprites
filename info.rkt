a2aa5ce44c559c62fac20cd15ad210cfdf7d9649
aBlender <JasonLongLe@gmail.com>
2020-02-03 13:49:12 -0800

    added game-engine-rpg dep

--- info.rkt
+++ info.rkt
@@ -1,7 +1,8 @@
 #lang info
 
 (define deps '("https://github.com/thoughtstem/pokemon-assets.git"
-               "https://github.com/thoughtstem/game-engine.git"))
+               "https://github.com/thoughtstem/game-engine.git"
+               "https://github.com/thoughtstem/game-engine-rpg.git"))
 
 (define build-deps '("scribble-lib"
                      "racket-doc"
